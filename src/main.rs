0f597aecff479544b03160450b4a36669373eef5
Iaroslav Sheptykin <i.sheptykin@neusta.de>
2023-01-12 15:46:11 +0100

    What if

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@
 // This code is editable, feel free to hack it!
 // You can always return to the original code by clicking the "Reset" button ->
 
+extern crate jsonpath_lib as jsonpath;
+extern crate serde_json;
+
+use jsonpath::Selector;
+use serde_json::Value;
+
 // This is the main function
 fn main() {
     // Statements here are executed when the compiled binary is called
@@ -13,16 +19,20 @@ fn main() {
     println!("{}", hello());
 }
 
-
-fn hello() -> &'static str {
-    "Hello, world!"
+fn json_path_value(json: &str, json_path: &str) -> Option<Value> {
+    let json: Value = serde_json::from_str(json).unwrap();
+    let selector = Selector::new(json_path).unwrap();
+    selector.find(&json).next().cloned()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn it_greets() {
-        assert_eq!("Hello, world!", hello());
+    fn it_can_jsonpath() {
+        let json = r#"{ "name": { "first": "John", "last": "Doe" }, "age": 30 }"#;
+        let json_path = "$.name.first";
+        let value = json_path_value(json, json_path);
+        assert_eq!(value, Some(json!("John")));
     }
 }
