bc4dfbc44ed8acef89d39234ba6c480bb2ada8c6
Iaroslav Sheptykin <i.sheptykin@neusta.de>
2023-01-19 21:12:11 +0000

    I have a function!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate jsonpath_lib as jsonpath;
+extern crate alloc;
 #[macro_use]
 extern crate serde_json;
 
@@ -6,59 +7,30 @@ fn main() {
     println!("ok");
 }
 
+fn getAuthors() -> alloc::string::String {
+    let ret = jsonpath::select_as_str(r#"
+    {
+        "school": {
+            "friends": [
+                    {"name": "친구1", "age": 20},
+                    {"name": "친구2", "age": 20}
+                ]
+        },
+        "friends": [
+            {"name": "친구3", "age": 30},
+            {"name": "친구4"}
+        ]
+    }
+    "#, "$..friends[0]").unwrap();
+
+    ret
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn it_can_jsonpath() {
-        let json_obj = json!({
-            "store": {
-                "book": [
-                    {
-                        "category": "reference",
-                        "author": "Nigel Rees",
-                        "title": "Sayings of the Century",
-                        "price": 8.95
-                    },
-                    {
-                        "category": "fiction",
-                        "author": "Evelyn Waugh",
-                        "title": "Sword of Honour",
-                        "price": 12.99
-                    },
-                    {
-                        "category": "fiction",
-                        "author": "Herman Melville",
-                        "title": "Moby Dick",
-                        "isbn": "0-553-21311-3",
-                        "price": 8.99
-                    },
-                    {
-                        "category": "fiction",
-                        "author": "J. R. R. Tolkien",
-                        "title": "The Lord of the Rings",
-                        "isbn": "0-395-19395-8",
-                        "price": 22.99
-                    }
-                ],
-                "bicycle": {
-                    "color": "red",
-                    "price": 19.95
-                }
-            },
-            "expensive": 10
-        });
-
-        let mut selector = jsonpath::selector(&json_obj);
-
-        assert_eq!(
-            selector("$.store.book[*].author").unwrap(),
-            vec![
-                "Nigel Rees",
-                "Evelyn Waugh",
-                "Herman Melville",
-                "J. R. R. Tolkien"
-            ]
-        );
+        assert_eq!(getAuthors(), r#"[{"name":"친구3","age":30},{"name":"친구1","age":20}]"#);
     }
 }
